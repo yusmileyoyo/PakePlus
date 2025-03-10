bda1d9a95162d9eea5391f3ee88b60af360e290c
yusmileyoyo <144758743+yusmileyoyo@users.noreply.github.com>
2025-03-10 19:44:43 +0800

    update custom js from pakeplus

--- src-tauri/data/custom.js
+++ src-tauri/data/custom.js
@@ -3,12 +3,3 @@ console.log(
     'color:orangered;font-weight:bolder'
 )
 
-const hookClick = (e) => {
-    e.preventDefault()
-    const origin = e.target.closest('a')
-    if (origin && origin.href) {
-        location.href = origin.href
-    }
-}
-
-document.addEventListener('click', hookClick, { capture: true })
